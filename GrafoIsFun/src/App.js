9755e1bec00af280a34127b99246bf7873100989
vitordiniz25 <vitordiniz25@gmail.com>
2021-08-06 15:57:31 -0300

    Criando escopo de funcoes

--- GrafoIsFun/src/App.js
+++ GrafoIsFun/src/App.js
@@ -1,24 +1,106 @@
 import './Styles.css';
-import React from 'react';
+import React, { useState } from 'react';
+import { initGraph } from './utils/createGraph';
 import { Stage, Layer, Arrow, Circle, Text } from "react-konva";
 
 function App() {
-  // function createGraph() {
-  //   if (vertex <= 0 || vertex > 1000 || isNaN(vertex)) {
-  //     alert("Número de vértices inválido!!!")
-  //     return
-  //   }
-  //   if (edges <= 0 || edges > 1000 || isNaN(edges)) {
-  //     alert("Número de arestas inválido!!!")
-  //     return
-  //   }
-  //   if (edges > vertex * (vertex - 1) && edges > 0) {
-  //     alert("Número de arestas inválido!!!\n Lembrete: Número de arestas é menor ou igual ao número de Vértices*(Vértices-1)")
+  const [graph_matrix, setGraph_matrix] = useState(null);
+  const [vertex, setVertex] = useState(0);
+  const [edges, setEdges] = useState(null);
+  const [randomGraphStyle, setRandomStyle] = useState([]);
+
+
+  function createGraph() {
+    if (vertex <= 0 || vertex > 1000 || isNaN(vertex)) {
+      alert("Número de vértices inválido!!!")
+      return
+    }
+    if (edges <= 0 || edges > 1000 || isNaN(edges)) {
+      alert("Número de arestas inválido!!!")
+      return
+    }
+    if (edges > vertex * (vertex - 1) && edges > 0) {
+      alert("Número de arestas inválido!!!\n Lembrete: Número de arestas é menor ou igual ao número de Vértices*(Vértices-1)")
+      return;
+    }
+
+    //Funçao de inicializar grafo aqui!!!
+    const graph = initGraph(vertex);
+    setGraph_matrix(graph);
+    generateGraphStyle();
+  }
+
+  const generateGraphStyle = (vertex) => {
+    const array = [];
+    for (let i = 0; i < vertex; i++) {
+      array.push({
+        top: Math.floor(Math.random() * 800),
+        left: Math.floor(Math.random() * 800),
+        position: "absolute",
+      })
+    }
+    setRandomStyle(array);
+  }
+
+  //   // function connectVertex(origin, destination) {
+  //   //   const isAlreadyConnected = verifyConnectivity(origin, destination)
+  //   //   if (isAlreadyConnected) {
+  //   //     alert("Vértices já conectados!")
+  //   //     return;
+  //   //   }
+
+  //   if (origin === destination) {
+  //     alert("Um vértice não pode se ligar a ele mesmo!")
   //     return;
   //   }
 
-  //   //Funçao de inicializar grafo aqui!!!
+  //   let newGraph = graph_matrix;
+  //   if (destination < graph_matrix.length) {
+  //     newGraph[origin][destination] = 1
+  //     setGraph_matrix(newGraph);
+  //     setEdgesCount(edgesCount - 1)
+  //     setDestinyVertex(0)
+  //     setOriginVertex(0)
+  //   } else {
+  //     alert("Essa posição não existe!")
+  //   }
   // }
+
+  // function connectVertexBothDirection(origin, destination) {
+  //   let newGraph = graph_matrix;
+
+  //   if (origin === destination) {
+  //     alert("Não pode ligar nele mesmo");
+  //     return;
+  //   }
+  //   if (destination < graph_matrix.length) {
+  //     let isOriginNotConnected = !verifyConnectivity(origin, destination);
+  //     let isDestinationNotConnected = !verifyConnectivity(destination, origin);
+
+  //     if (isOriginNotConnected && isDestinationNotConnected) {
+  //       newGraph[origin][destination] = 1;
+  //       newGraph[destination][origin] = 1;
+  //       setGraph_matrix(newGraph);
+  //       setEdgesCount(edgesCount - 2);
+  //     } else if (isOriginNotConnected) {
+  //       newGraph[origin][destination] = 1;
+  //       setGraph_matrix(newGraph);
+  //       setEdgesCount(edgesCount - 1);
+  //     } else if (isDestinationNotConnected) {
+  //       newGraph[destination][origin] = 1;
+  //       setGraph_matrix(newGraph);
+  //       setEdgesCount(edgesCount - 1);
+  //     }
+  //     setDestinyVertex(0);
+  //     setOriginVertex(0);
+  //   } else {
+  //     alert("POSIÇÃO NÃO EXISTENTE");
+  //   }
+  // }
+
+  function finishGraph() {
+    // função para executar funcionalidade de achar o menor caminho
+  }
 
   const graphInfoForm = () => (
     <div className="inputForm">
@@ -37,7 +119,7 @@ function App() {
         <button>adicionar</button>
       </div>
     </div>
-  )
+  );
 
   const graphSearch = () => (
     <div className="inputSearch">
@@ -48,11 +130,58 @@ function App() {
         <button>procurar</button>
       </div>
     </div>
-  )
+  );
+
+  // const renderEdges = (graph) => {
+  //   const connectedEdges = [];
+
+  //   for (let i = 0; i < vertex; i++) {
+  //     for (let j = 0; j < vertex; j++) {
+  //       if (graph[i][j] === 1) {
+  //         connectedEdges.push(
+  //           <Arrow
+  //             points={[
+  //               randomGraphStyle[i].left,
+  //               randomGraphStyle[i].top,
+  //               randomGraphStyle[j].left,
+  //               randomGraphStyle[j].top,
+  //             ]}
+  //             fill="black"
+  //             stroke="black"
+  //           />
+  //         );
+  //       }
+  //     }
+  //   }
+  //   return connectedEdges;
+  // };
+
+  // const renderVertex = (graph) => {
+  //   const vertexes = graph.map((vertex, i) => (
+  //     <>
+  //       <Circle
+  //         radius={5}
+  //         x={randomGraphStyle[i].left}
+  //         y={randomGraphStyle[i].top}
+  //         stroke="black"
+  //         fill="black"
+  //       />
+  //       <Text
+  //         x={randomGraphStyle[i].left - 3}
+  //         y={randomGraphStyle[i].top + 10}
+  //         text={i}
+  //         fontSize={20}
+  //         fontStyle="bold"
+  //       />
+  //     </>
+  //   ));
+
+  //   return vertexes;
+  // };
 
   const renderGraph = () => (
     <div className="graph-container">
-      <Stage width={900} height={900} className="graphView">
+      <Stage width={918} height={670} className="graphView">
         {/* <Layer>
           {renderEdges(graph_matrix)}
           {renderVertex(graph_matrix)}
@@ -68,6 +197,7 @@ function App() {
       </div>
       {graphInfoForm()}
       {graphSearch()}
+      {renderGraph()}
     </div>
   );
 }
